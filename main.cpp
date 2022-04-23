b390ffcb96c49a56c1a26e52a22f4d023115ede8
Akshay Raman <ar10021161@gmail.com>
2022-04-23 21:11:02 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -107,7 +107,7 @@ void compareTrees(HashTree *htold, HashTree *htnew) {
 }
 
 int main() {
-  cout << "BackUp With HashTrees" << endl;
+  cout << "(Parallel) Data Backup Using Hash Trees" << endl;
 
   cout << endl;
 
